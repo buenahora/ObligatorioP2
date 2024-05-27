d397710cfa0171fa5b2ccf2b1d9330f39b4b7ef7
buenahora <buenahorafranco@gmail.com>
2024-05-27 11:18:19 -0300

    menu funcional

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import classes.Cancion;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.AbstractList;
 import java.util.ArrayList;
 //import java.util.List;
 //import java.util.Objects;
@@ -12,57 +13,98 @@ import java.util.Scanner;
 public class Main {
 
     public static ArrayList<Cancion> leerCanciones() {
-        ArrayList<Cancion> canciones = new ArrayList<>();
-        File csvFile = new File("src/universal_top_spotify_songs.csv");
+       return null;
+    }
 
-        try (Scanner scanner = new Scanner(csvFile)) {
-            //Saltea la primera linea, si no se rompe
-            scanner.nextLine();
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                line = line.replaceAll(";", "");
-                line = line.substring(1, line.length() - 1);
-                line = line.replaceAll("\"\"", "\"");
-
-                String[] values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-
-                for (int i = 0; i < values.length; i++) {
-                    values[i] = values[i].replaceAll("[;\"]", "");
-                }
-
-                if (values[0].equals("7hDoxkN20lLb06zifzYnD2")) {
-                    line = "7hDoxkN20lLb06zifzYnD2,\"Ishq - From \"\"Lost	Found\"\"\",\"Faheem Abdullah, Rauhan Malik\",\"2\",\"0\",\"0\",\"PK\",\"2024-05-13\",\"75\",\"False\",\"228639\",\"Ishq (From \"\"Lost	Found\"\")\",\"2024-03-09\",\"0.383\",\"0.547\",\"4\",\"-8.862\",\"0\",\"0.0275\",\"0.423\",\"0.0466\",\"0.147\",\"0.236\",\"95.068\",\"4\"";
-                    line = line.replaceAll(";", "");
-                    line = line.replaceAll("\"\"", "\"");
-                    values = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
-
-                    for (int i = 0; i < values.length; i++) {
-                        String str = values[i];
-                        str = str.replaceAll("[;\"]", "");
-                        line = line.trim();
-                        values[i] = str;
-                    }
-
-                    Cancion cancion = new Cancion(values[0], values[1], values[2], Integer.parseInt(values[3]), Integer.parseInt(values[4]), Integer.parseInt(values[5]), values[6], values[7], Integer.parseInt(values[8]), Boolean.parseBoolean(values[9]), Integer.parseInt(values[10]), values[11], values[12], Double.parseDouble(values[13]), Double.parseDouble(values[14]), Integer.parseInt(values[15]), Double.parseDouble(values[16]), Integer.parseInt(values[17]), Double.parseDouble(values[18]), Double.parseDouble(values[19]), Double.parseDouble(values[20]), Double.parseDouble(values[21]), Double.parseDouble(values[22]), Double.parseDouble(values[23]), Integer.parseInt(values[24]));
-                    canciones.add(cancion);
-
-                } else {
-                    Cancion cancion = new Cancion(values[0], values[1], values[2], Integer.parseInt(values[3]), Integer.parseInt(values[4]), Integer.parseInt(values[5]), values[6], values[7], Integer.parseInt(values[8]), Boolean.parseBoolean(values[9]), Integer.parseInt(values[10]), values[11], values[12], Double.parseDouble(values[13]), Double.parseDouble(values[14]), Integer.parseInt(values[15]), Double.parseDouble(values[16]), Integer.parseInt(values[17]), Double.parseDouble(values[18]), Double.parseDouble(values[19]), Double.parseDouble(values[20]), Double.parseDouble(values[21]), Double.parseDouble(values[22]), Double.parseDouble(values[23]), Integer.parseInt(values[24]));
-                    canciones.add(cancion);
-//
-                }
-
+    public static AbstractList<Cancion> top10CancionesPaisFecha(ArrayList<Cancion> canciones, String pais, String fecha) {
+        ArrayList<Cancion> cancionesPaisFecha = new ArrayList<>();
+        for (Cancion cancion : canciones) {
+            if (cancion.getCountry().equals(pais) && cancion.getSnapshot_date().equals(fecha)) {
+                cancionesPaisFecha.add(cancion);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
-        return canciones;
+        return cancionesPaisFecha;
+    }
+
+    public static AbstractList<Cancion> top5CancionesMasTop50Fecha(ArrayList<Cancion> canciones, String fecha) {
+        ArrayList<Cancion> cancionesMasTop50 = new ArrayList<>();
+        for (Cancion cancion : canciones) {
+            if (cancion.getSnapshot_date().equals(fecha)) {
+                cancionesMasTop50.add(cancion);
+            }
+        }
+        return cancionesMasTop50;
+    }
+
+    public static AbstractList<Cancion> top7ArtistasMasTop50Fecha(ArrayList<Cancion> canciones, String fechaInicio, String fechaFin) {
+        ArrayList<Cancion> cancionesMasTop50 = new ArrayList<>();
+        for (Cancion cancion : canciones) {
+            if (cancion.getSnapshot_date().equals(fechaInicio) || cancion.getSnapshot_date().equals(fechaFin)) {
+                cancionesMasTop50.add(cancion);
+            }
+        }
+        return cancionesMasTop50;
+    }
+
+    public static int cantidadVecesArtistaTop50Fecha(ArrayList<Cancion> canciones, String artista, String fecha) {
+        int cantidad = 0;
+        for (Cancion cancion : canciones) {
+            if (cancion.getArtists().contains(artista) && cancion.getSnapshot_date().equals(fecha)) {
+                cantidad++;
+            }
+        }
+        return cantidad;
+    }
+
+    public static int cantidadCancionesTempoRangoFecha(ArrayList<Cancion> canciones, double tempoMin, double tempoMax, String fechaInicio, String fechaFin) {
+        int cantidad = 0;
+        for (Cancion cancion : canciones) {
+            if (cancion.getTempo() >= tempoMin && cancion.getTempo() <= tempoMax && (cancion.getSnapshot_date().equals(fechaInicio) || cancion.getSnapshot_date().equals(fechaFin))) {
+                cantidad++;
+            }
+        }
+        return cantidad;
     }
 
     public static void main(String[] args) {
-        ArrayList<Cancion> canciones = leerCanciones();
-        System.out.println(canciones.size());
+//        ArrayList<Cancion> canciones = leerCanciones();
+        Scanner sc = new Scanner(System.in);
+        int option;
 
+        do {
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Top 10 canciones en un país en un día dado");
+            System.out.println("2. Top 5 canciones que aparecen en más top 50 en un día dado");
+            System.out.println("3. Top 7 artistas que más aparecen en los top 50 para un rango de fechas dado");
+            System.out.println("4. Cantidad de veces que aparece un artista específico en un top 50 en una fecha dada");
+            System.out.println("5. Cantidad de canciones con un tempo en un rango específico para un rango específico de fechas");
+            System.out.println("0. Salir");
+
+            option = sc.nextInt();
+
+            switch (option) {
+                case 1:
+                    // top10CancionesPaisFecha();
+                    break;
+                case 2:
+                    // Aquí va la lógica para la opción 2
+                    break;
+                case 3:
+                    // Aquí va la lógica para la opción 3
+                    break;
+                case 4:
+                    // Aquí va la lógica para la opción 4
+                    break;
+                case 5:
+                    // Aquí va la lógica para la opción 5
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, intente de nuevo.");
+                    break;
+            }
+        } while (option != 0);
     }
+
 }
